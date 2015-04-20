e4153b781eb951ed07bc6f27dde6a8d33ea03ab2
D'alberto Pierre-Julien <dalberto.pj@gmail.com>
2015-04-20 17:54:34 +0200

    Circle on the maps

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -62,7 +62,7 @@ angular.module('app')
 
     var mapOptions = {
       center: myLatlng,
-      zoom: 16,
+      zoom: 14,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     var map = new google.maps.Map(document.getElementById("map"),
@@ -84,6 +84,19 @@ angular.module('app')
     google.maps.event.addListener(marker, 'click', function() {
       infowindow.open(map,marker);
     });
+
+    var circleOptions = {
+      strokeColor: '#FF0000',
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: '#FF0000',
+      fillOpacity: 0.35,
+      map: map,
+      center: myLatlng,
+      radius: 1000
+    };
+    // Add the circle for this city to the map.
+    cityCircle = new google.maps.Circle(circleOptions);
 
     $scope.map = map;
   }
